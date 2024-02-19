f092a4f958cf29cddd75a1b10aaa5d78628f0303
aminRmc <ramcilovicamin@gmail.com>
2024-02-19 14:43:49 +0100

    add clear app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <section className='w-[100%]'>
       
-    </>
+    </section>
   )
 }
 
